7a14b7de7252ad984f192f8c027e669279035f20
Dylan Fridman <dylanfridman@gmail.com>
2019-05-11 03:00:05 -0300

    Merged code with Juli and fixed hasLoops

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import copy
 from collections import deque
 
 class Formula:
-    def __init__(self, t, children, text):
-        self.symbol = t
+    def __init__(self, symbol, children, text):
+        self.symbol = symbol
         self.children = children
         self.text = text
 
@@ -71,6 +72,7 @@ class DGraph:
                 self.isOrphan[ar.name] = False
 
     def hasLoops(self):
+        visited = set()
         ancestors_per_node = dict(map(lambda y: (y, set()), self.nodes))
 
         f = lambda x: self.isOrphan[x]
@@ -85,6 +87,7 @@ class DGraph:
 
         while len(to_visit) > 0:
             parent = self.nodes[to_visit.popleft()]
+            visited.add(parent.name)
 
             for child in parent.arrowsTo:
                 if child.name in ancestors_per_node[child.name]:
@@ -94,7 +97,7 @@ class DGraph:
                 ancestors_per_node[child.name].update(ancestors_per_node[parent.name])
                 to_visit.append(child.name)
 
-        return False
+        return not len(visited) == len(self.nodes)
 
 class ProofState:
     def __init__(self):
@@ -112,54 +115,56 @@ class Rule:
 def feetea(f, a, S): #determina si se puede meter cosas adentro de f (en las variables que empiezan con caracter en S) para llegar a a.
     aAndpList(f, S)
     aAndpListAux(a, S)
-    buildTheGraph(S)
-    #chequeo si no quedo con loops
-    #rellenar vacion con nuevas a_i
-    #masticar
-    return feeteaAux(f, a, S)
+    if feeteaAux(f, a, S):
+        buildTheGraph(S)
+        print(len(G.nodes['a1'].arrowsTo))
+        #chequeo si no quedo con loops
+        fillGaps('a')
+        chew(S)
+        return True
+    else:
+        return False
 
 def feeteaAux(f, a, S):
-    if not f.symbol[0] in S:
-        if not a.symbol[0] in S:
-            if not f.symbol == a.symbol:
-                #print('casito 1')
+    if not f.symbol[0] in S: #si el adam de f es un simbolo del sistema.
+        if not a.symbol[0] in S: #si el adam de a es un simbolo del sistema.
+            if not f.symbol == a.symbol: #si los adams de f y a son distintos.
                 return False
-            else:
+            else: #si los adams de f y a son iguales.
                 for i in range(len(f.children)):
-                    if feeteaAux(f.children[i], a.children[i], S) == False:
+                    if not feeteaAux(f.children[i], a.children[i], S):
                         return False
-                #chequear loops (o al final?) antes de returnear True
                 return True
-        else:
-            if extension[a.symbol] == []:
+        else: #si el adam de a no es un simbolo del sistema.
+            if extension[a.symbol] == []: #si no quedó nadie en la metavariable de a.
                 extension[a.symbol].append(f)
                 return True
-            else:
+            else: #si sí quedó alguien en la metavariable de a.
                 return feeteaAux(f, extension[a][0])
-    else:
-        if not a.symbol[0] in S:
-            if extension[f.symbol] == []:
+    else: #si el adam de f no es un simbolo del sistema.
+        if not a.symbol[0] in S: #si el adam de a es un simbolo del sistema.
+            if extension[f.symbol] == []: #si no quedó nadie en la metavariable de f.
                 extension[f.symbol].append(a)
                 return True
-            else:
-                return feeteaAux(a, extension[f][0])
-        else:
-            if f.symbol == a.symbol:
+            else: #si si quedó alguien en la metavariable de f.
+                return feeteaAux(extension[f][0], a)
+        else: #si el adam de a no es un simbolo del sistema.
+            if f.symbol == a.symbol: #si son la misma metavariable.
                 return True
-            else:
-                if extension[f.symbol] == [] and extension[a.symbol] == []:
+            else: #si no son la misma metavariable.
+                if extension[f.symbol] == [] and extension[a.symbol] == []: #si no quedó nadie en la metavariable de f ni en la de a.
                     if f.symbol == sorted([f.symbol, a.symbol])[0]:
                         extension[f.symbol].append(a)
                         return True
                     else:
                         extension[a.symbol].append(f)
                         return True
-                elif not extension[f.symbol] == [] and extension[a.symbol] == []:
+                elif not extension[f.symbol] == [] and extension[a.symbol] == []: #si quedó alguien en la metavariable de f y no quedó nadie en la de a.
                     extension[a.symbol].append(f)
-                elif extension[f.symbol] == [] and not extension[a.symbol] == []:
+                elif extension[f.symbol] == [] and not extension[a.symbol] == []: #si quedó alguien en la metavariable de a y no quedó nadie en la de f.
                     extension[f.symbol].append(a)
-                else:
-                    return feeteaAux(extension[f.symbol], extension[a.symbol])
+                else: #si quedó alguien en la metavariable de f y alguien en la de a.
+                    return feeteaAux(extension[f.symbol][0], extension[a.symbol][0])
 
 def aAndpList(f, S): #dada una formula f devuelve la lista de todas las variables que ocurren (? en f y empiezan con un caracter en S (generalmente S es ['a', 'p']).
     global extension
@@ -181,11 +186,14 @@ def buildTheGraph(S):
     for k in extension:
         if not extension[k] == []:
             d[k] = GraphNode(k)
+    for k in extension:
+        if not extension[k] == []:
+            d[k] = GraphNode(k)
             global l
             l = []
             buildTheGraphAux(extension[k][0], S)
             d[k].arrowsTo = l.copy()
-    print('d:', d)
+    #print('d:', d)
     G = DGraph(d)
 
 def buildTheGraphAux(N, S):
@@ -193,6 +201,47 @@ def buildTheGraphAux(N, S):
         l.append(d[N.symbol])
     for ch in N.children:
         buildTheGraphAux(ch, S)
+
+def fillGaps(s): #mira extension y los a los valores que tienen su lista vacia, le agregan una nueva a_i (s es la letra que que quiero que se agregue con indices, en este caso 'a').
+    global newVars
+    newVars = []
+    i = 1
+    for k in extension:
+        if len(extension[k]) == 0:
+            while s+str(i) in extension:
+                i += 1
+            extension[k].append(Formula(s+str(i), [], s+str(i)))
+            newVars.append(s+str(i))
+            i += 1
+
+#OJO QUE EL EJEMPLO FUNCO JOYA PERO TIRABA UNA ITERACION DE MAS. CHEQUEAR
+def chew(S): #cuando extension tiene exactamente un elemento por lista, esta funcion va agregando capas de elementos que cada uno es igual al anterior pero reemplazando cada variable que empiece con caracter en S por lo que tiene esa variable en la capa anterior de su lista en extension.
+    finished = {}
+    numFinished = 0
+    global extLayers
+    extLayers = 1
+    for k in extension:
+        finished[k] = False
+    while numFinished < len(extension):
+        for k in extension:
+            extension[k].append(chewAux(copy.deepcopy(extension[k][-1])))
+            if extLayers > 1:
+                if extension[k][-1].text == extension[k][-2].text and not extension[k][-2].text == extension[k][-3].text:
+                    numFinished += 1
+            else:
+                if extension[k][-1].text == extension[k][-2].text:
+                    numFinished += 1
+        extLayers += 1
+
+def chewAux(f):
+    if f.symbol in extension:
+        f = copy.deepcopy(extension[f.symbol][extLayers - 1])
+    else:
+        for ch in f.children:
+            chewAux(ch)
+    return f
+
+
 
 f = Formula('^', [], '')
 f.children.append(Formula('A', [], ''))
@@ -203,7 +252,7 @@ f2 = Formula('->', [], '')
 f2.children.append(Formula('a0', [], ''))
 f2.children.append(Formula('a1', [], ''))
 f2.updateText()
-# print(f2.text)
+#print(f2.text)
 
 f3 = Formula('V', [], '')
 f3.children.append(Formula('^', [], ''))
@@ -269,14 +318,23 @@ t15.toTree()
 t16 = Formula('', [], 'V(A2,A1)')
 t16.toTree()
 
+t17 = Formula('', [], '-->(a1,-->(p1,a2))')
+t17.toTree()
+
+t18 = Formula('', [], '-->(p1,-->(a2,a3))')
+t18.toTree()
+
+
 n1 = GraphNode('n1')
 n2 = GraphNode('n2')
 n3 = GraphNode('n3')
 n4 = GraphNode('n4')
+n5 = GraphNode('n5')
 
 n1.arrowsTo.append(n2)
-n2.arrowsTo.append(n3)
 n3.arrowsTo.append(n4)
+n4.arrowsTo.append(n5)
+n5.arrowsTo.append(n1)
 
-gr = DGraph({'n1': n1,'n2': n2, 'n3': n3, 'n4': n4})
+gr = DGraph({'n1': n1,'n2': n2, 'n3': n3, 'n4': n4, 'n5': n5})
 print(gr.hasLoops())
